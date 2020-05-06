19b6fcfc6178f2ac6846e207d5d377276a236fdd
teamfalcon <team-falcon@vw-dilab.com>
2020-05-06 11:01:45 +0100

    Add StatsJS to measure FPS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Stats from "stats.js";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const stats = new Stats();
+stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
+stats._counter = 0;
+document.body.appendChild( stats.dom );
+
+function animate() {
+	stats.begin();
+	stats._counter++;
+	stats.end();
+	requestAnimationFrame( animate );
+}
+animate();
 
 ReactDOM.render(
   <React.StrictMode>
