5bf6fb851525a70338433230ceb5bc426a9486b9
teamfalcon <team-falcon@vw-dilab.com>
2020-06-24 22:45:35 +0100

    Start with <App>

--- src/PrimeChecker.4.jsx
+++ src/PrimeChecker.4.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React from "react";
 import isPrimeNumber from "./isPrimeNumber";
 import { Huge as origHuge} from './Components';
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import Invitations from './invitations/Invitations';
 // import FPSMonitor from "./FPSMonitor";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Invitations dealer={{ name: "John Wick", id: 123}} />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
